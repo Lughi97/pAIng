42cacc81081cb7367830479b934b0ae077766528
Luca <lucaghisi.lg@gmail.com>
2024-09-02 11:09:27 +0200

    Clean agent and disk code
    Added comments, removed  old oonx file and set the only one usefull

--- Assets/Scripts/Disk.cs
+++ Assets/Scripts/Disk.cs
@@ -1,83 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Class handels:
+/// 1- Collision handeling when the disk collide with the wall or paddle
+/// </summary>
 public class Disk : MonoBehaviour
 {
     public float speed = 15;
     public Vector3 direction;
-    private Rigidbody rb;
+    public Rigidbody rb;
     public bool OutOfBounds;
     private Vector3 startPosition;
-  
 
-    //Start the disk movement at a random direction.
-    void Start()
+    private void Start()
     {
-        
-        rb = GetComponent<Rigidbody>();
-        int startRandomDirection = Random.Range(0, 360);
-        direction = new Vector3(Mathf.Cos(startRandomDirection * Mathf.Deg2Rad), 0f, Mathf.Sin(startRandomDirection * Mathf.Deg2Rad));
-        startPosition = transform.position;
-        //rb.velocity = rb.velocity.normalized * speed;
-        rb.velocity = direction * speed;
+       startPosition = transform.position;
     }
 
-    private void SpeedCheck()
-    {
-        if (rb.velocity.magnitude < speed)
-        {
-            rb.velocity = direction * speed;
-        }
-    }
-    // check the collision between the wall or the paddle.
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Wall")
         {
-
-            BounceOffWall(collision.contacts[0].normal);
+            direction = Vector3.Reflect(direction, collision.contacts[0].normal);
+            float randomDeflectAngle = Random.Range(-5f, 5f)*Mathf.Deg2Rad;
+            // calculate the new outgoing direction.
+            float directionX = direction.x * Mathf.Cos(randomDeflectAngle) - direction.z * Mathf.Sin(randomDeflectAngle);
+            float directionZ = direction.x * Mathf.Sin(randomDeflectAngle) + direction.z * Mathf.Cos(randomDeflectAngle);
+            direction = new Vector3(directionX, 0f, directionZ).normalized;
+            rb.velocity = direction * speed;
+            
         }
+
         if (collision.gameObject.tag == "Paddle")
         {
-            BounceOffPaddle(collision.contacts[0].normal);
+            direction = Vector3.Reflect(direction, collision.contacts[0].normal).normalized;
+            rb.velocity = direction * speed;
         }
     }
-    // calculate the new outgoing direction based on the normal of the collision contact with the wall.
-    private void BounceOffWall(Vector3 normal)
-    {
-        Vector3 incomingDirection = direction;
-        direction = Vector3.Reflect(direction, normal);
-        float incomingAngle = Mathf.Atan2(incomingDirection.z, incomingDirection.x) * Mathf.Rad2Deg;
-        // Calculate the new outgoing direction of the disk.
-        float randomDeflectAngle = Random.Range(-5f, 5f) * Mathf.Deg2Rad;
-        // use the 2d rotation matrix.
-        float directionX = direction.x * Mathf.Cos(randomDeflectAngle) - direction.z * Mathf.Sin(randomDeflectAngle);
-        float directionZ = direction.x * Mathf.Sin(randomDeflectAngle) + direction.z * Mathf.Cos(randomDeflectAngle);
 
-        direction = new Vector3(directionX, 0f, directionZ).normalized;
-        float outgoingAngle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
-        //apply the new direction with the same speed in order keep energy.
-        rb.velocity = direction * speed;
-        SpeedCheck();
-        // Log the incoming and outgoing angles
-       // Debug.Log($"Incoming angle: {incomingAngle:F2} degrees");
-       // Debug.Log($"Outgoing angle: {outgoingAngle:F2} degrees");
-    }
-
-    // calcualte the new outgoing direction based on the normal of the collision contact with the paddle.
-    private void BounceOffPaddle(Vector3 normal)
-    {
-       
-        direction = Vector3.Reflect(direction, normal).normalized;
-        rb.velocity = direction * speed;
-        SpeedCheck();
-
-    }
-
-    // Function called when the enviroment is reseted. The disk goes out of bounds.
-    // is selected a new direction of the disk.
-    public void RestDisk()
+    public void ResetDiskDirection()
     {
         if (OutOfBounds)
             OutOfBounds = false;
@@ -87,7 +49,6 @@ public class Disk : MonoBehaviour
         rb.velocity = direction * speed;
     }
 
-    // trigger that checks if the disk is out of bounds.
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Bound" || other.gameObject.tag == "BoundPaddle")
--- Assets/Scripts/PaddleAgent.cs
+++ Assets/Scripts/PaddleAgent.cs
@@ -5,81 +5,97 @@ using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
 
+/// <summary>
+/// Class handles:
+/// 1- Collecting observation.
+/// 2- Define the action needed to reach the goal
+/// 3- Set positve or negative reward based on the taken action.
+/// 4- Reset the Epsiode when the disk goes out of bounds or the Max setp
+///    for the episode is reached.
+/// </summary>
 public class PaddleAgent : Agent
 {
-    public Transform diskTransform;
+    public Transform diskTransform; 
     
-    public float speed = 10f;
+    public float currentSpeed = 0f;
+    public float minSpeed = 5f;
+    public float maxSpeed = 25f; 
+  
     public Vector3 startPosition;
-    public bool resetFirstHit = true;
-    public float proximityThreshold = 0.5f;
-
+    
+    public float zDistanceThreshold = 0.5f; // minimal thershold z cooridnate  in order to get a reward.
+    private float optimalSpeed = 2f; // speed ranges in which agent is rewared.
 
     
     void Start()
     {
         startPosition = transform.position;
     }
+
     public override void OnEpisodeBegin()
     {
         transform.position = startPosition;
         RestDisk();
     }
-    /*get the just the x and z position and velocity of the disk in order to match the paddle during training*/
+
     public override void CollectObservations(VectorSensor sensor)
     {
-        // disk & paddle position
+       // Collect position information for both disk and paddle.
         sensor.AddObservation(transform.position.z);
         sensor.AddObservation(diskTransform.position.x);
         sensor.AddObservation(diskTransform.position.z);
-        //disk speed
+
+        // Collect speed information for both disk and paddle.
         sensor.AddObservation(diskTransform.GetComponent<Rigidbody>().velocity.x);
         sensor.AddObservation(diskTransform.GetComponent<Rigidbody>().velocity.z);
+        sensor.AddObservation(currentSpeed);
 
     }
-    // reset disk position calculate a new direction
+
     private void RestDisk()
-    {
-       
-        diskTransform.GetComponent<Disk>().RestDisk();
+    { 
+        diskTransform.GetComponent<Disk>().ResetDiskDirection();
     }
 
-    // Set up the agent behavior and 
     public override void OnActionReceived(ActionBuffers actions)
     {
-       ;
-        //move in z direction
         float moveZ = actions.ContinuousActions[0];
-        transform.position += new Vector3(0, 0, moveZ) * Time.deltaTime * speed;
-       
-        // 
-        float zDistanceToDistk = Mathf.Abs(diskTransform.position.z- transform.position.z);
-        if(zDistanceToDistk <= proximityThreshold && Mathf.Abs(diskTransform.position.x - transform.position.x)<5)
-        {
-            SetReward(0.1f);
-        }
+        currentSpeed = Mathf.Clamp(Mathf.Clamp(actions.ContinuousActions[1], 0, 1) * maxSpeed, minSpeed, maxSpeed);
+
+        transform.position += new Vector3(0, 0, moveZ) * currentSpeed * Time.deltaTime;
+
+
+        float zDistanceToDisk = Mathf.Abs(diskTransform.position.z - transform.position.z);
+      
+        float speedVariance = Mathf.Abs(diskTransform.GetComponent<Rigidbody>().velocity.magnitude - currentSpeed);
+ 
+
+        if (zDistanceToDisk <= zDistanceThreshold && Vector3.Distance(diskTransform.position, transform.position)<5)
+            SetReward(0.1f); // the agent is close enough to the disk
+
+        if (speedVariance <= optimalSpeed)
+            SetReward(0.5f); // the agent almost the same speed of the disk
+
     }
+
     private void Update()
     {
-        // disk goes out of bounds.
-        if (diskTransform.GetComponent<Disk>().OutOfBounds|| Vector3.Distance(transform.position, diskTransform.position) > 13f)
+        if (diskTransform.GetComponent<Disk>().OutOfBounds || Vector3.Distance(transform.position, diskTransform.position) > 13f)
         {
-          
-            SetReward(-1f);
-           // RestDisk();
+            SetReward(-1f); // penilize the agent when the disk goes out of bounds.
             EndEpisode();
         }
     
     }
    
-    //Check if the paddle manages to hit the dik
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Disk")
         {
-            SetReward(1f);
+            SetReward(1f); // reard the agent when it hits the disk
            
         }
     }
+
 
 }
